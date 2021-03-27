1903bb67b9d364552e73b5392a25621c51a22a43
CELALETTIN ALTINTAS <celalettinaltnts@gmail.com>
2021-03-27 10:42:29 +0300

    Added Sample Codes

---
+++ ImplicitOperator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ImplicitOperator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Celalettin",
+                MiddleName = "Timoqi",
+                LastName = "Altintas"
+            };
+
+            CustomerDto customerDto = customer;
+
+            Console.WriteLine(customerDto.ToString());
+            Console.ReadKey();
+        }
+    }
+
+
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    internal class CustomerDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+
+        public static implicit operator CustomerDto(Customer customer)
+        {
+            return new CustomerDto
+            {
+                Id = customer.Id,
+                FullName = $"{customer.FirstName} {customer.MiddleName} {customer.LastName}"
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} : {FullName}";
+        }
+    }
+}
